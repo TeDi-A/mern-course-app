020797cdd518c45bc7874eeeea8bfa7e0ca2ae20
TeDi-A <tediadjene@gmail.com>
2024-05-10 23:53:08 +0100

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const subjects = require('./subjects/subjects')
 const Class = require('./schema/ClassSchema')
 const classRoutes = require('./routes/classRoutes')
 
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static(__dirname + "/dist"))
+
 app.get('/', (req, res) => {
     res.send("Hello");
 })
