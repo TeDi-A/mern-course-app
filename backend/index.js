0dc57362aa666fe31dbfa8655caee2593d039ba8
TeDi-A <109079331+TeDi-A@users.noreply.github.com>
2024-05-11 01:14:54 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect(atlasConnectionUri, {
     dbName: 'subjects'
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
