96a42b951b260452ee075b35d4660e4739021a1f
TeDi-A <109079331+TeDi-A@users.noreply.github.com>
2024-05-11 01:11:55 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,10 @@ mongoose.connect(atlasConnectionUri, {
     dbName: 'subjects'
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is Running at ${PORT}`);
 });
