760f2775feb6149e049bd48a6de5cf11d2dd59b9
TeDi-A <109079331+TeDi-A@users.noreply.github.com>
2024-06-30 13:58:30 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,20 +18,11 @@ mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB Atlas');
 });
 
-
 app.use(cors({
     origin: "https://divcourses.vercel.app", 
     credentials: true, 
     optionsSuccessStatus: 200
 }));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://divcourses.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
