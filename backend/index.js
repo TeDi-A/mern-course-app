25e57b3ea69155cb8052d96ba6d97e80ac365e30
TeDi-A <109079331+TeDi-A@users.noreply.github.com>
2024-07-01 03:19:58 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Exposed-Headers:", "Set-Cookie");
+    res.header("Access-Control-Exposed-Headers", "Set-Cookie");
     next();
 });
 
